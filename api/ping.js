a70c48591ed8b6a86062258f19ca7c988c7546fd
Sunylia <sunyliaapp@gmail.com>
2025-10-08 14:47:40 +0200

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,12 @@
 export default function handler(req, res) {
+  // CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');           // à restreindre plus tard
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   return res.status(200).json({ ok: true, path: '/api/ping' });
 }
